819f97c243cd2a4f07eb09a2732a9ff9ec8dd219
thanushiii <thanushi.20230930@iit.ac.lk>
2025-03-24 21:59:16 +0530

    refactor: remove  title from Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ const Header = ({ children, className }: HeaderProps) => {
   return (
     <div className={cn("header", className)}>
       
-        <span className="text-xl font-semibold text-white">cloudSync</span>
+        
       
       {children}
     </div>
